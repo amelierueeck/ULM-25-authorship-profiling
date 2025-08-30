5ca764c9b278b46d80b13a72b25786564f60b45d
kobrue02 <konrad.brg@gmail.com>
2025-08-30 07:24:45 +0000

    add bucketing option

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,25 +1,41 @@
 from datasets import load_dataset
 
-def get_data_splits(to_csv=False, columns=["text", "age", "gender"]):
+def get_data_splits(to_csv=False, columns=["text", "age", "gender"], buckets=True, bins=None):
     """
     Load the blog authorship corpus dataset and split it into training, validation, and test sets.
     Args:
         to_csv (bool): If True, save the splits as CSV files.
         columns (list): List of columns to select from the dataset.
+        buckets (bool): If True, convert ages to bucket labels.
+        bins (list): Age bin boundaries for bucketing.
     Returns:
         tuple: DataFrames for training, validation, and test sets.
     """
     dataset = load_dataset("barilan/blog_authorship_corpus", trust_remote_code=True)
-    data_train, data_test = dataset["train"], dataset["validation"] # use the validation split as test
+    data_train, data_test = dataset["train"], dataset["validation"]
     data_train, data_val = data_train.train_test_split(test_size=0.1).values()
+    
+    if buckets:
+        bins = bins or [0, 18, 23, 27, 33, 37, 43, 47, 53, 57, 100]
+        
+        def bucketize_age(example):
+            example["age"] = int(np.digitize(example["age"], bins) - 1)
+            return example
+        
+        data_train = data_train.map(bucketize_age)
+        data_val = data_val.map(bucketize_age)
+        data_test = data_test.map(bucketize_age)
+    
     if columns:
         data_train = data_train.select_columns(columns)
         data_val = data_val.select_columns(columns)
         data_test = data_test.select_columns(columns)
+    
     if to_csv:
         data_train.to_pandas().to_csv("data/data_train.csv", index=False)
         data_val.to_pandas().to_csv("data/data_val.csv", index=False)
         data_test.to_pandas().to_csv("data/data_test.csv", index=False)
+    
     return data_train, data_val, data_test
 
 data_train, data_val, data_test = get_data_splits(to_csv=True)
